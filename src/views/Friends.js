76da31d658bbb7511e43379402857bb48ceaaa18
gomamon <leeye9779@gmail.com>
2018-11-28 04:22:21 +0900

    make chatting with friends page

--- src/views/Friends.js
+++ src/views/Friends.js
@@ -1,5 +1,6 @@
-import DayPicker from '@enact/moonstone/DayPicker';
 import Divider from '@enact/moonstone/Divider';
+import Input from '@enact/moonstone/Input';
+import Picker from '@enact/moonstone/Picker';
 import Layout, {Cell} from '@enact/ui/Layout';
 import React from 'react';
 import Scroller from '@enact/moonstone/Scroller';
@@ -7,21 +8,72 @@ import Button from '@enact/moonstone/Button/Button';
 import axios from 'axios';
 
 
-const FriendsView = (props) => (
-    
-	<Layout orientation="vertical">
-		<Cell component={Scroller} focusableScrollbar>
-			<Divider>Default</Divider>
-			<DayPicker
-				noneText="none"
-				title="Friends"
-			/>
-			<input type ="text" id ="token"></input>
-			<Button onClick={props.login}>Load Chats</Button>
-			<p id="devList"></p>
-		</Cell>
-	</Layout>
-);
+const friendlist = ["INA", "INO", "YEDDO"];
+const chatlist = [
+    {
+        title:"hihi",
+        body: "hohoho"
+    },
+      {
+        title: "hoho",
+        body: "who'll win?"
+      },
+      {
+        title: "I have something to tell you",
+        body: "get out!"
+      },
+];
+class MessageList extends React.Component {
+	render() {
+	  return (
+		<ul className="message-list">                 
+		  {this.props.messages.map(message => {
+			return (
+			 <li key={message.id}>
+			   <div>
+				 {message.title}
+			   </div>
+			   <div>
+				 {message.body}
+			   </div>
+			 </li>
+		   )
+		 })}
+	   </ul>
+	  )
+	}
+}
 
+class FriendsView extends React.Component {
+
+    constructor() {
+		super()
+		this.state = {
+		   messages: chatlist
+		}
+	  }
+    render(){
+        return(
+            <Layout orientation="vertical">
+                <Cell shrink components="label">
+                    <div>
+                    {/* send message */}
+                    <Input placeholder="Enter your message" dismissOnEnter/>
+                    <Picker
+                        orientation="horizontal"
+                        width="medium"
+                    >
+                        {friendlist}
+                    </Picker>
+                    </div>
+                </Cell>
+
+                <Cell component={Scroller} focusableScrollbar>
+                    <MessageList messages={this.state.messages} />
+                </Cell>
+            </Layout>
+        );
+    }
+}
 
 export default FriendsView;
