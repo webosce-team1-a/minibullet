597b3d5755f9f4a079350953f9d22c728eb86a29
ina-uzu <ina.uzu.song@gmail.com>
2018-11-28 00:33:05 +0900

    ina

--- src/views/Friends.js
+++ src/views/Friends.js
@@ -3,7 +3,14 @@ import Divider from '@enact/moonstone/Divider';
 import Layout, {Cell} from '@enact/ui/Layout';
 import React from 'react';
 import Scroller from '@enact/moonstone/Scroller';
-import { Button } from '@enact/moonstone/Button/Button';
+import Button from '@enact/moonstone/Button/Button';
+import axios from 'axios';
+
+const tokenURL ="https://api.pushbullet.com/v2/users/me";
+const baseURL = "https://api.pushbullet.com/v2/";
+const userURL = baseURL + "chats";
+const deviceURL = baseURL + "devices";
+const pushURL = baseURL + "pushes";
 
 const FriendsView = (props) => (
     
@@ -13,8 +20,15 @@ const FriendsView = (props) => (
 			<DayPicker
 				noneText="none"
 				title="Friends"
-			/>
-			<Button onClick={props.testFunction}>TEST</Button>		
+			/>	
+
+			< Button onClick={() => function(){
+				 var tok = "o.jkutNveC2uQZtTfPNfG0GchnrM1xdVpx";
+				 axios.post(tokenURL, { "Access-Token": tok }).then(res =>{
+					 console.log(res.email);
+				 })
+			}}>TEST</Button>
+
 		</Cell>
 	</Layout>
 );
