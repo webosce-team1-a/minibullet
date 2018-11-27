8d3713e40d7f7204755c1ae48cb84db5789c74e1
ina-uzu <ina.uzu.song@gmail.com>
2018-11-28 02:08:45 +0900

    initial commit

--- src/App/App.js
+++ src/App/App.js
@@ -6,7 +6,6 @@ import Layout, {Cell} from '@enact/ui/Layout';
 import MoonstoneDecorator from '@enact/moonstone/MoonstoneDecorator';
 import PropTypes from 'prop-types';
 import React from 'react';
-
 import ScrollerComponent from '@enact/moonstone/Scroller';
 import ViewManager from '@enact/ui/ViewManager';
 import axios from 'axios';
@@ -28,13 +27,12 @@ const views = [
 	{title:'friends', view: Friends},
 	{title:'setting', view:Setting},
 ]
-let loginflag = true;
-let firstflag = true;
 
-let tok = "o.I7mba7PcOUawRGIHHYCW5QheducyLqDY";
+let tok = "o.jkutNveC2uQZtTfPNfG0GchnrM1xdVpx";
 
 const getChatList = () => {
 	const url = "https://api.pushbullet.com/v2/" + "chats";
+	console.log(tok);
 	let extra = { headers : { 'Access-Token': tok} };
 	var out = document.getElementById("chatList");
 
@@ -47,6 +45,23 @@ const getChatList = () => {
 		.catch(error => {
 			console.error(error);
 		});
+}
+
+const getDevices = () =>{
+	const url =  "https://api.pushbullet.com/v2/" + "devices";
+	let extra = { headers : { 'Access-Token': tok} };
+	var out = document.getElementById("devList");
+
+	axios.get(url, extra)
+		.then(response => {
+			var devList = response.request.device;
+			for(var i = 0; i < devList.length; i++)
+                out.innerHTML += devList[i].nickname + "<br>";
+		})
+		.catch(error => {
+			console.error(error);
+		});
+	
 }
 
 let flag = true;
@@ -72,8 +87,7 @@ const AppBase = kind({
 
 	render: ({handleChange, index, ...rest}) => {
 		delete rest.onChange;
-		
-		
+
 		return (
 			<Layout {...rest}>
 				{flag ? (<Cell component={ScrollerComponent} size="20%">
@@ -81,9 +95,9 @@ const AppBase = kind({
 						{views.map((view) => view.title)}
 					</Group>
 				</Cell> ) : null}
-
 				<Cell component={ViewManager} index={index}>
 					{views.map((view, i) => {
+						view['getDevices'] = getDevices;
 						view['getChat'] = getChatList;
 						view['token'] = tok;
 						return (
@@ -100,42 +114,3 @@ const App = MoonstoneDecorator(Changeable({prop: 'index', change: 'onChange'}, A
 
 
 export default MoonstoneDecorator(App);
-	{/*
-			<Layout {...rest}>
-				{flag ? (<Cell component={ScrollerComponent} size="20%">
-					<Group childComponent={Item} itemProps={{className: css.navItem}} onSelect={handleChange} select={'radio'} selected={index}>
-						{views.map((view) => view.title)}
-					</Group>
-				</Cell> ) : null}
-
-				<Cell component={ViewManager} index={index}>
-					{views.map((view, i) => {
-						view['getChat'] = getChatList;
-						view['token'] = tok;
-						return (
-							<View {...view} key={i} />
-						);
-					})}
-				</Cell>
-			</Layout>
-		);
-	}
-
-
-
-					{firstflag ? 
-					(<Cell component={ViewManager} index={0}>
-					{views.map((view, i) => {
-						firstflag = false;
-						return(<View {...view} key={i} />);
-					})} </Cell>)
-					
-					: 
-					
-					(<Cell component={ViewManager} index={index}>
-					{views.map((view, i) => {
-						firstflag = false;
-						return(<View {...view} key={i} />);
-					})} </Cell>)
-
-					*/}
