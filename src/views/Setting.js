a24861e8e912049281e8ac739073fc54e094b189
gomamon <leeye9779@gmail.com>
2018-11-28 11:42:49 +0900

    잡혀라에러

--- src/views/Setting.js
+++ src/views/Setting.js
@@ -4,17 +4,21 @@ import React from 'react';
 import ToggleButton from '@enact/moonstone/ToggleButton';
 import Scroller from '@enact/moonstone/Scroller';
 import Toggleable from '@enact/ui/Toggleable';
-
+import beanbird from './beanbird.png';
 const StatefulToggleButton = Toggleable({toggle: 'onClick', prop: 'selected'}, ToggleButton);
 
 
 const SettingView = () => (
  
-        <Layout orientation="vertical" align="center">
-            <Cell component={Scroller} focusableScrollbar>
-                <StatefulToggleButton toggleOffLabel="logout" toggleOnLabel="login" />
-            </Cell>
-        </Layout>
+    <Layout orientation="vertical" align="center">
+        <Cell shirink align="center">
+            <StatefulToggleButton toggleOffLabel="logout" toggleOnLabel="login" />
+        </Cell>
+        <Cell shirink align="right">
+            <img src={beanbird} width="300px" height="300px"></img>
+        </Cell>
+
+    </Layout>
   
 );
 
