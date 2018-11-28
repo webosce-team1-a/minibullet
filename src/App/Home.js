eb5840ab86db690f4ad2340c2e342d2ed9bc98c8
gomamon <leeye9779@gmail.com>
2018-11-28 14:19:33 +0900

    make more cool

--- src/App/Home.js
+++ src/App/Home.js
@@ -6,12 +6,13 @@ import Layout, {Cell} from '@enact/ui/Layout';
 const Home = () => (
       <Layout orientation="vertical" align="center">
             <Cell shirink align="center">
-                  <img src={pushmon} center sizing="fit"></img> 
-            </Cell>
-            <Cell shirink align="center">
-                  <br/>
-                  <p style={{position: 'center'}}>HI. I'm Pushmon!</p> 
-                  <p style={{position: 'center'}}>You can exchange files, text with not only your devices but also friends.</p> 
+                  <div>
+                  <img src={pushmon} center width="80%"></img> 
+                  </div>
+                  <div>
+                        <p style={{position: 'center'}}>HI. I'm Pushmon!</p> 
+                        <p style={{position: 'center'}}>You can exchange files, text with not only your devices but also friends.</p> 
+                  </div>
             </Cell>
       </Layout>
 );
--- src/views/Setting.js
+++ src/views/Setting.js
@@ -5,23 +5,77 @@ import ToggleButton from '@enact/moonstone/ToggleButton';
 import Scroller from '@enact/moonstone/Scroller';
 import Toggleable from '@enact/ui/Toggleable';
 import beanbird from './beanbird.png';
+import Button from '@enact/moonstone/Button';
+import Notification from '@enact/moonstone/Notification';
+
 const StatefulToggleButton = Toggleable({toggle: 'onClick', prop: 'selected'}, ToggleButton);
 
 
-const SettingView = () => (
- 
-    <Layout orientation="vertical" align="center">
-        <Cell shirink align="center">
-            <StatefulToggleButton toggleOffLabel="logout" toggleOnLabel="login" />
-        </Cell>
-        <Cell shirink align="center">
-        </Cell>
-        <Cell shirink align="center">
-            <img src={beanbird} width="300px" height="300px"></img>
-        </Cell>
+class SettingView extends React.Component{
+    constructor (props) {
+        super(props);
+        
+		this.state = {
+			open: false
+		};
 
-    </Layout>
-  
-);
+		this.handleOpen = this.handleOpen();
+		this.handleClose = this.handleClose();
+	}
+ 
+
+	handleOpen = () => {
+		this.setState({
+			['open']: true
+		});
+    }
+    
+
+	handleClose = () => {
+		this.setState({
+			['open']: false
+		});
+	}
+
+    render(){
+
+        const {open} = this.state;
+
+        return(
+            <Layout orientation="vertical">
+                
+                <Cell shirink aline="center">
+                    <Divider>logout button</Divider>
+                    <div>
+                        <StatefulToggleButton toggleOffLabel="logout" toggleOnLabel="login" />
+                    </div>
+                    <br/>
+                    <Divider>You can check your information</Divider>
+                    <div>
+                    <Button onClick={this.handleOpen}>My information</Button>
+                    <Notification
+                        open={open}
+                        onClose={this.handleClose}
+                    >
+                        <span>{`Not to worry, this message isn't going to be very long.
+                        It just has to be long enough to show what a long message looks like.
+                        That's all; have a nice day.`}</span>
+                        <buttons>
+                            <Button onClick={this.handleClose}>OK!</Button>
+                        </buttons>
+                    </Notification>
+
+                    </div>
+                    
+                    
+                </Cell>
+                <Cell shirink align="center">
+                    <img src={beanbird} width="300px" height="300px"></img>
+                </Cell>
+
+            </Layout>
+        )
+    }
+};
 
 export default SettingView;
