ebb069a8082a20fb55db78b3c2e7aa355c32ad29
ina-uzu <ina.uzu.song@gmail.com>
2018-11-28 09:05:08 +0900

    nono

--- src/views/Me.js
+++ src/views/Me.js
@@ -7,45 +7,18 @@ import React from 'react';
 import ToggleButton from '@enact/moonstone/ToggleButton';
 import VirtualList from '@enact/moonstone/VirtualList';
 import Scroller from '@enact/moonstone/Scroller';
+import axios from 'axios';
+import { Button } from '@enact/ui/Button/Button';
 
 	/*items.push('Item ' + ('00' + i).slice(-3));*/
-
-const devices = ["android", "chrome"];
-
 const chatlist = [
-	{
-	  senderId: "perborgen",
-	  text: "who'll win?"
-	},
-	{
-	  senderId: "janedoe",
-	  text: "who'll lost?"
-	},
-	{
-	senderId: "janedoe",
-	text: "who'll lost?"
-	},
-	{
-	senderId: "janedoe",
-	text: "who'll lost?"
-	},
-	{
-	senderId: "janedoe",
-	text: "who'll lost?"
-	},
-	{
-		senderId: "janedoe",
-		text: "who'll lost?"
-	},
-	{
-		senderId: "janedoe",
-		text: "who'll lost?"
-	},
-	{
-		senderId: "janedoe",
-		text: "who'll lost?"
-	},
-  ]
+	{	
+		id:"0",
+	senderId: "perborgen",
+	text: "who'll win?"
+	}
+]
+
 {/*
 class SendMessageForm extends React.Component {
 	constructor() {
@@ -83,31 +56,30 @@ class SendMessageForm extends React.Component {
 	}
 }
 */}
+
 class MessageList extends React.Component {
 	render() {
-	  return (
+		return (
 		<ul className="message-list">                 
 		  {this.props.messages.map(message => {
 			return (
-			 <li key={message.id}>
-			   <div>
-				 {message.senderId}
+			 <li>
+			   <div id="title">
 			   </div>
-			   <div>
-				 {message.text}
+			   <div id="body">
 			   </div>
 			 </li>
 		   )
 		 })}
 	   </ul>
-	  )
+	  );
 	}
 }
 
 
-class MeView extends React.Component {
-	constructor() {
-		super()
+class MeView extends React.Component{
+	constructor(props) {
+		super(props)
 		this.state = {
 		   messages: chatlist
 		}
@@ -115,32 +87,70 @@ class MeView extends React.Component {
 
 
 	render(){
-		return (
+		const url =  "https://api.pushbullet.com/v2/" + "devices";
+		let tok = this.props.token;
+		let extra = { headers : { 'Access-Token': tok} };
+		let devices=[];
+		let devName;
+
+		axios.get(url, extra)
+			.then(response => {
+				var devList = response.data.devices;
+				for(var i = 0; i < devList.length; i++){
+					if(devList[i].nickname !=null){
+						devices.push(devList[i].nickname);
+					}
+				}
+				devName = devices;
+				
+				return( 
+					<Layout orientation="vertical">
+					<Cell shrink components="label">
+						<div>
+						{/* send message */}
+			
+						<p>{this.devName}</p>
+						<input type ="text" id ="body" placeholder="Enter your message"></input>
+						{/*<Input type ="text" id="body" placeholder="Enter your message" dismissOnEnter/>*/}
+		
+		
+						<Button onClick={this.props.pushMe}>PUSH</Button>
+		
+						<Picker
+							orientation="horizontal"
+							width="medium"
+						>
+						{diveces}
+						</Picker>
+						</div>
+					</Cell>
+				</Layout>
+				);				
+			})
+			.catch(error => {
+				console.error(error);
+			});
+			
+	}
+
+	show(){
+		
+		return(
 			<Layout orientation="vertical">
 			<Cell shrink components="label">
-				<div>
-				{/* send message */}
-				<Input placeholder="Enter your message" dismissOnEnter/>
-				<Picker
-					orientation="horizontal"
-					width="medium"
-				>
-					{devices}
-				</Picker>
-				</div>
-			</Cell>
-
+			<div>
+					
 			{/* render message*/}
 			<Cell component={Scroller} focusableScrollbar>
 				<MessageList messages={this.state.messages} />
 			</Cell>
-		</Layout>
+			</div>
+			</Cell>
+			</Layout>
 		);
 	}
+
 }
-
-
-
 
 
 export default MeView;
